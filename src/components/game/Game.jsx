eee3d74e392af7eabb208120277f84096712d0aa
Swygax <kennykysv@gmail.com>
2024-03-07 12:54:26 -0300

    Aumentei o tamanho das estrelas para ser visivel em todo tipo de tela

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -19,9 +19,9 @@ export default function Game() {
 
     // Adicionar camadas de partículas em um array
     const particlesArray = [
-      createParticles(0x0000ff, 0.1, 1000),
-      createParticles(0xff00ff, 0.1, 1000),
-      createParticles(0xffffff, 0.1, 1000),
+      createParticles(0xffffff, 2, 1000),
+      createParticles(0x0000ff, 1, 5000),
+      createParticles(0xff00ff, 0.5, 10000),
     ];
 
     // Adicionar as partículas à cena
